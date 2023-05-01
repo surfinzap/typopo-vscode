11da1141849584dfec70822423663d315fe8d956
surfinzap <surfinzap@users.noreply.github.com>
2023-05-01 13:16:50 +0200

    ♻️ Code cleanup

--- extension.js
+++ extension.js
@@ -1,4 +1,3 @@
-// @ts-nocheck
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
@@ -52,9 +51,6 @@ function activate(context) {
 }
 
 
-
-
-exports.activate = activate;
 
 // this method is called when your extension is deactivated
 function deactivate() {}
