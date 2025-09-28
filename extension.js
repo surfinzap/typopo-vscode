5645ad587925457d64a4b5215ac6dfceaad9d787
Brano Sandala <surfinzap@users.noreply.github.com>
2025-09-28 19:55:10 +0200

    1.4.0 (#11)

--- extension.js
+++ extension.js
@@ -4,50 +4,49 @@ const vscode = require('vscode');
 const typopo = require('typopo');
 let language = 'en-us';
 let config = {
-	removeLines: false,
+  removeLines: false,
 };
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	context.subscriptions.push(
-		vscode.commands.registerCommand('typopo-vscode.fixTypos', function () {
-			// Get the active text editor
-			const editor = vscode.window.activeTextEditor;
-			
-			if (editor) {
-				const document = editor.document;
-				const position = editor.selection.active;
+  context.subscriptions.push(
+    vscode.commands.registerCommand('typopo-vscode.fixTypos', function () {
+      // get the active text editor
+      const editor = vscode.window.activeTextEditor;
 
-				let selection = editor.selection;
-				let text = document.getText(selection);
+      if (editor) {
+        const document = editor.document;
 
-				// if no text is selected, then select the line
-				if (text == '') {
-					let lineRange = editor.document.lineAt(position).range;
-					selection = new vscode.Selection(lineRange.start, lineRange.end);
-					text = document.getText(selection)				
-				}
-				
-				// check for user config
-				const extensionConfig = vscode.workspace.getConfiguration('typopo');
-				
-				language = extensionConfig.get('language');
+        // check for user config
+        const extensionConfig = vscode.workspace.getConfiguration('typopo');
+
+        language = extensionConfig.get('language');
         config.removeLines = extensionConfig.get('removeLines');
         config.removeWhitespacesBeforeMarkdownList = extensionConfig.get('removeWhitespacesBeforeMarkdownList');
         config.keepMarkdownCodeBlocks = extensionConfig.get('keepMarkdownCodeBlocks');
-				
-				const fixedText = typopo.fixTypos(text, language, config)
-				editor.edit(editBuilder => {
-					editBuilder.replace(selection, fixedText);
-				});
-			}
-		})
-	);
+
+        
+        editor.edit(editBuilder => {
+          editor.selections.forEach(selection => {
+            let text = document.getText(selection);
+
+            // if no text is selected, then select the line
+            if (text === '') {
+              const position = selection.active;
+              let lineRange = editor.document.lineAt(position).range;
+              selection = new vscode.Selection(lineRange.start, lineRange.end);
+              text = document.getText(selection);
+            }
+
+            const fixedText = typopo.fixTypos(text, language, config);
+            editBuilder.replace(selection, fixedText);
+          });
+        });
+      }
+    })
+  );
 }
 
 
@@ -56,6 +55,6 @@ function activate(context) {
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
+  activate,
+  deactivate
 }
